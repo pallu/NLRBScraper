2513b24b3bd8010cb001f365ccc897a08569cd48
Pallab Gupta <pallu.geo@yahoo.com>
2024-07-05 23:48:54 -0400

    cookie container

--- NLRBScraper/Program.cs
+++ NLRBScraper/Program.cs
@@ -1,24 +1,29 @@
 ﻿using AngleSharp;
 using AngleSharp.Dom;
 using AngleSharp.Html.Dom;
+using System.Net;
 using System.Web;
 
 namespace NLRBScraper
 {
     internal class Program
     {
+        static string baseHref = "https://www.nlrb.gov";
         static string url = "https://www.nlrb.gov/advanced-search";
+        static CookieContainer cookieContainer = new CookieContainer();
         static void Main(string[] args)
         {
-            var retInit = GetInitial().Result;
-            Pass2(retInit).Wait();
+            using var handler = new HttpClientHandler() { CookieContainer = cookieContainer };
+            using var httpClient = new HttpClient(handler);
+            var retInit = GetInitial(httpClient).Result;
+            Pass2(httpClient, retInit).Wait();
             Console.WriteLine("Hello, World!");
         }
 
-        static async Task<(string formBuildId, string formId)> GetInitial()
+        static async Task<(string formBuildId, string formId)> GetInitial(HttpClient httpClient)
         {
             (string formBuildId, string formId) elForms = new();
-            using var httpClient = new HttpClient();
+            //using var httpClient = new HttpClient();
             var httpResponseMessage = await httpClient.GetAsync(url);
             if (httpResponseMessage.IsSuccessStatusCode)
             {
@@ -42,9 +47,9 @@ namespace NLRBScraper
             }
             return elForms;
         }
-        static async Task Pass1((string formBuildId, string formId) input)
+        static async Task Pass1(HttpClient httpClient, (string formBuildId, string formId) input)
         {
-            using var httpClient = new HttpClient();
+            //using var httpClient = new HttpClient();
             var formVariables = new List<KeyValuePair<string, string>>();
             formVariables.Add(new ("foia_report_type", "cases_and_decisions"));
             formVariables.Add(new ("form_build_id", input.formBuildId));
@@ -65,9 +70,9 @@ namespace NLRBScraper
             }
             
         }
-        static async Task Pass2((string formBuildId, string formId) input)
+        static async Task Pass2(HttpClient httpClient, (string formBuildId, string formId) input)
         {
-            using var httpClient = new HttpClient();
+            //using var httpClient = new HttpClient();
             var builder = new UriBuilder(url);
             var query = HttpUtility.ParseQueryString(builder.Query);
             //foia_report_type=cases_and_decisions&search_term=&&from_date=2019-01-01&to_date=2024-07-03&case_status=-1&case_type=-1&&&&sort_by=date_filed&order_by=desc&items_per_page=20&submit=Search&reset=Reset&form_build_id=form-SmrK3JEafoCLMV_7_FNKn9bWskTrDj4cquUcruMMo88&form_id=nlrb_foia_search_form&op=Search
@@ -96,12 +101,16 @@ namespace NLRBScraper
                     var body = await response.Content.ReadAsStringAsync();
                     IConfiguration configuration = Configuration.Default;
                     IBrowsingContext context = BrowsingContext.New(configuration);
-                    IDocument document = await context.OpenAsync(req => req.Content(body));
+                    IDocument document = await context.OpenAsync(req => req.Content(body).Address(new Url(baseHref)));
                     var dnld = document.QuerySelectorAll<IHtmlAnchorElement>("*").Where(x => x.Attributes["id"] is not null && x.Attributes["id"].Value == "ads-download-button").FirstOrDefault();
                     if(dnld is not null)
                     {
                         var href = dnld.Href;
-
+                        var resp2 = await httpClient.GetAsync(href);
+                        if (resp2.IsSuccessStatusCode) 
+                        { 
+                            
+                        }
                     }
                 }
             }
